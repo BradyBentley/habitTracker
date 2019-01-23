a49c957c8361aff888258deb2f2383ce99f17d1d
David Truong <truongvd@gmail.com>
2019-01-22 21:26:06 -0800

    Update AddHabit storyboard

---
+++ HabitTracker/Habit/ViewControllers/NewHabit/NewHabitViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NewHabitViewController.swift
+//  HabitTracker
+//
+//  Created by David Truong on 1/22/19.
+//  Copyright © 2019 HabitGroup. All rights reserved.
+//
+
+import UIKit
+
+class NewHabitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Properties
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
