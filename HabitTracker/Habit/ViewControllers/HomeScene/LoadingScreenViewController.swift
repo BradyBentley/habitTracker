e2b941e389575e9c35598c334429c90feb68ac8a
karissalynncodes <karissalm223@gmail.com>
2019-01-31 14:49:18 -0700

    adds loading view controller

---
+++ HabitTracker/Habit/ViewControllers/HomeScene/LoadingScreenViewController.swift
@@ -0,0 +1,85 @@
+//
+//  LoadingScreenViewController.swift
+//  HabitTracker
+//
+//  Created by Karissa McDaris on 1/31/19.
+//  Copyright © 2019 HabitGroup. All rights reserved.
+//
+
+import UIKit
+
+class LoadingScreenViewController: UIViewController {
+    
+    let homeTabBarController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+    
+    @IBOutlet weak var shadowWidth: NSLayoutConstraint!
+    
+    @IBOutlet weak var shadowHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var floatingManAnimation: UIImageView!
+    
+    @IBOutlet weak var shrinkingShadowAnimation: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //dismiss(animated: true, completion: nil)
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        animateOpeningPage()
+        animatingOfShadow()
+        self.present(homeTabBarController!, animated: true, completion: nil)
+        //let homeVC = self.storyboard?.instantiateViewController(withIdentifier: "AddHabitViewController") as! AddHabitViewController
+        //self.present(homeVC, animated: true, completion: nil)
+        //self.presentingViewController?.dismiss(animated: false, completion:nil)
+    }
+    
+    func animateOpeningPage(){
+        floatingManAnimation.center.y = view.center.y
+        
+        UIView.animate(withDuration: 1.5, delay: 0,  options: [.repeat, .autoreverse], animations: ({
+            self.floatingManAnimation.center.y = self.view.center.y + 10
+            
+        }), completion: nil)
+    }
+    
+    func animatingOfShadow() {
+        
+        Timer.scheduledTimer(withTimeInterval: 10, repeats: false) { (_) in
+            self.present(self.homeTabBarController!, animated: true, completion: nil)
+        }
+    
+        shadowWidth.constant = 100
+        self.shrinkingShadowAnimation.setNeedsLayout()
+        UIView.animate(withDuration: 1.5, delay: 0, options:[.autoreverse, .repeat], animations: ({
+            self.shrinkingShadowAnimation.layoutIfNeeded()
+        }), completion: nil)
+        
+        shadowHeight.constant = 21
+        self.shrinkingShadowAnimation.setNeedsLayout()
+        UIView.animate(withDuration: 1.5, delay: 0, options: [.autoreverse, .repeat], animations: ({
+            self.shrinkingShadowAnimation.layoutIfNeeded()
+     
+        }), completion: { success in
+//
+            print("adsf")
+        })
+        
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
